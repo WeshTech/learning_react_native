8764f33574fed23ff08e977653fc9b83b0157405
Engineer David <davidwaweru2500@gmail.com>
2025-04-17 12:20:39 -0400

    chapter

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style = {styles.text}>Hello there!!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: '#25292e',
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#d35400",
+  }
+})
