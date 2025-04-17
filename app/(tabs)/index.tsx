66a0f5d42cb042c3f7fa2cd415b8b26cbcd5b574
Engineer David <davidwaweru2500@gmail.com>
2025-04-17 13:52:33 -0400

    chapter2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native';
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
-
-export default function HomeScreen() {
+export default function Index() {
   return (
-    <View>
-      <Text>Hello there!!</Text>
+    <View
+      style={styles.container}
+    >
+      <Text style = {styles.text}>Hello there!!!</Text>
+      <Link href = "/about" style = {styles.button}>About</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: '#25292e',
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#d35400",
+  },
+  button: {
+    padding: 5,
+    backgroundColor: "#f4d03f",
+    margin: 10,
+    borderRadius: 5,
+  }
+})
